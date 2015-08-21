d9d28403c0f1de5164e177af2c7bd0be53a5d1f5
Christian Jandl Mac <dm141534@fhstp.ac.at>
2015-08-21 17:04:07 +0200

    photo hinzufügen klappt bis preview
    code von video noch löschen

--- demo/src/main/java/com/daimajia/slider/demo/DetailView.java
+++ demo/src/main/java/com/daimajia/slider/demo/DetailView.java
@@ -3,12 +3,21 @@ package com.daimajia.slider.demo;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -23,6 +32,11 @@ import com.daimajia.slider.demo.AppController;
 import com.daimajia.slider.demo.model.Task;
 import com.daimajia.slider.demo.adater.VolleyImageLoader;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class DetailView extends Activity {
 
     private static final String TAG = DetailView.class.getSimpleName();
@@ -30,6 +44,15 @@ public class DetailView extends Activity {
     // tasks json url
     private static final String url = "http://dm141534.students.fhstp.ac.at/phototask_api/api/tasks/" ;
     private static final String preview_url = "http://dm141534.students.fhstp.ac.at/phototask_api/";
+
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    //pictures are stored in folder
+    private static final String IMAGE_DIRECTORY_NAME = "Phototask";
+    //Uri to store image
+    private Uri fileUri;
+    private ImageView imgPreview;
+    private Button btnCapturePicture, btnRecordVideo;
 
 
     Task task = new Task();
@@ -49,6 +72,8 @@ public class DetailView extends Activity {
         final TextView plate = (TextView) findViewById(R.id.detail_plate);
         final TextView date = (TextView) findViewById(R.id.detail_date);
 
+        imgPreview = (ImageView) findViewById(R.id.imgPreview);
+        btnCapturePicture = (Button) findViewById(R.id.add_pics);
         mNetworkImageView = (NetworkImageView) findViewById(R.id.preview_image);
 
         Intent i = getIntent();
@@ -118,8 +143,108 @@ public class DetailView extends Activity {
         });
         AppController.getInstance().addToRequestQueue(taskRequest);
     }
+/**
+ * Capture image button click event
+ * */
+public void captureImage(View view){
+    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+    fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
 
+    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+    // start the image capture Intent
+    startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+
+}
+    /**
+     * Receiving activity result method will be called after closing the camera
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // if the result is capturing Image
+        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // successfully captured the image
+                // display it in image view
+                previewCapturedImage();
+            } else if (resultCode == RESULT_CANCELED) {
+                // user cancelled Image capture
+                Toast.makeText(getApplicationContext(),
+                        "User cancelled image capture", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                // failed to capture image
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    /*
+     * Display image from a path to ImageView
+     */
+    private void previewCapturedImage() {
+        try {
+            // hide video preview
+
+            imgPreview.setVisibility(View.VISIBLE);
+
+            // bimatp factory
+            BitmapFactory.Options options = new BitmapFactory.Options();
+
+            // downsizing image as it throws OutOfMemory Exception for larger
+            // images
+            options.inSampleSize = 8;
+
+            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(),
+                    options);
+
+            imgPreview.setImageBitmap(bitmap);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Creating file uri to store image/video
+     */
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+    /*
+ * returning image / video
+ */
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+                        + IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + timeStamp + ".jpg");
+        } else  {
+            return null;
+        }
+
+        return mediaFile;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
